a720fb1bdbb920931b0db3b30eb64ccd64f75b09
GodisDeadx <107815486+GodisDeadx@users.noreply.github.com>
2023-11-10 19:29:07 -0500

    Delete tests directory

--- tests/breakpoint.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main() {    
-    int i {1};
-    std::cout << "Breaking The Matrix" << '\n';
-}
--- tests/vars.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main() {
-    int i{5};
-    char j{'c'};
-    long k{10};
-
-    std::cout << i << ' ' << j << ' ' << k << '\n';
-}
